bb8a5cb341ebe04f3028a232b880ce9867a451c6
mauroantonioserrano <maurocerano@gmail.com>
2022-11-27 16:44:19 -0300

    cree una nueva base de datos y añadi funciones a la tabla

--- productos.py
+++ productos.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import tkinter.font as tkFont
 from tkinter import ttk
 import sqlite3 
-from tkinter import*
+from tkinter import *
 class Aplicacion(Frame):
     def __init__(self, marco):
         super().__init__(marco)
@@ -10,75 +10,125 @@ class Aplicacion(Frame):
         self.pack()
         self.crear_componentes()
         self.obtener_productos()
-        self.agregar_productos()
-        self.editar_productos()
 
-  
+        self.editar_productos()
+        self.base_de_datos()
+
+    def base_de_datos(self):
+        conexion=sqlite3.connect("supermercado.db")
+        cursor=conexion.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS tienda(id INTEGER NOT NULL,nombre TEXT NOT NULL,marca TEXT,descripcion TEXT,precio REAL NOT NULL,cantidad INTEGER NOT NULL,PRIMARY KEY(id AUTOINCREMENT) )")
+        conexion.commit()
+        conexion.close()
 
 
     def obtener_productos(self):
-        conexion=sqlite3.connect("basededatosdesupermercado.db")
+        conexion=sqlite3.connect("supermercado.db")
         cursor=conexion.cursor()
-        obtener=cursor.execute("SELECT * FROM Producto")
-        # ob=cursor.fetchall()
-        # for i in ob:
-        #     print(i)
+        cur=cursor.execute("SELECT * FROM tienda")
+        # a=cursor.fetchall()
+        # for i in a:
+        #    print(i)
         hijos=self.tabla.get_children()
+        for h in hijos:
+            self.tabla.delete(h)
 
-        for e in hijos:
-            self.tabla.delete(e)
-
-        for i in obtener: 
-            self.tabla.insert("",0,text=i[1],values=i[2])
-
+        for i in cur:
+            self.tabla.insert("","end",text=i[0] ,values=(i[1],i[2],i[3],i[4],i[5]))
+        
         conexion.close()
-
-
+   
 
     def agregar_productos(self):
-        conexion=sqlite3.connect("basededatosdesupermercado.db")
+        conexion=sqlite3.connect("supermercado.db")
         cursor=conexion.cursor()
 
-        nom=self.txtNombre.get()
-        pre=self.precio.get()
+        id=self.id.get()
+        print(id)
 
-        lista=[(None,nom,pre)]
-        cursor.executemany("INSERT INTO Producto VALUES(?,?,?)",lista)
+        nombre=self.nombre.get()
+        print(nombre)
+
+        marca=self.marca.get()
+        print(marca)
+
+        descripcion=self.descripcion.get()
+        print(descripcion)
+
+        precio=self.precio.get()
+        print(precio)
+        cantidad=self.cantidad.get()
+        print(cantidad)
+
+        lista=[(id,nombre,marca,descripcion,precio,cantidad)]
+
+        cursor.executemany("INSERT INTO tienda VALUES(?,?,?,?,?,?)",lista)
         conexion.commit()
         conexion.close()
+
         self.obtener_productos()
+        
+
+
+        # conexion=sqlite3.connect("basededatosdesupermercado.db")
+        # cursor=conexion.cursor()
+
+        # nom=self.txtNombre.get()
+        # pre=self.precio.get()
+
+        # lista=[(None,nom,pre)]
+        # cursor.executemany("INSERT INTO Producto VALUES(?,?,?)",lista)
+        # conexion.commit()
+        # conexion.close()
+        # self.obtener_productos()
+        pass
 
     
     
     
     def eliminar_productos(self):
-        d=self.tabla.item(self.tabla.selection())['text']
-        
-        conexion = sqlite3.connect("basededatosdesupermercado.db")
-        cursor = conexion.cursor()
-        cursor.execute(f"DELETE FROM Producto WHERE  nombre='{d}'")
+
+        conexion=sqlite3.connect("supermercado.db")
+        cursor=conexion.cursor()    
+        id=self.tabla.item(self.tabla.selection())["text"]
+        cursor.execute(f"DELETE FROM tienda WHERE id=={id}")
         conexion.commit()
         conexion.close()
         self.obtener_productos()
+        # d=self.tabla.item(self.tabla.selection())['text']
+        
+        # conexion = sqlite3.connect("basededatosdesupermercado.db")
+        # cursor = conexion.cursor()
+        # cursor.execute(f"DELETE FROM Producto WHERE  nombre='{d}'")
+        # conexion.commit()
+        # conexion.close()
+        # self.obtener_productos()
+        pass
 
 
     def editar_productos(self):
-        
-
-        conexion = sqlite3.connect("basededatosdesupermercado.db")
-        cursor=conexion.cursor()
+        # n=self.tabla.item(self.tabla.selection())['text']
+        # v=self.tabla.item(self.tabla.selection())['values']
+        # print(n)
+        # print(v)
+        # conexion = sqlite3.connect("basededatosdesupermercado.db")
+        # cursor=conexion.cursor()
 
         n=self.tabla.item(self.tabla.selection())["text"]
-        # p=self.tabla.item(self.tabla.selection())['values'][0]
-        # print(p)
-        nom=self.n_nombre.get()
-        pre=self.n_precio.get()
-        print(pre)
+        p=self.tabla.item(self.tabla.selection())
 
-        cursor.execute(f'UPDATE Producto SET nombre="{nom}",precio="{pre}" WHERE nombre="{n}"')
-        conexion.commit()
-        conexion.close()
-        self.obtener_productos()
+        
+    
+        # # print(p)
+        # nom=self.n_nombre.get()
+        # pre=self.n_precio.get()
+        # print(pre)
+
+        # cursor.execute(f'UPDATE Producto SET nombre="{nom}",precio="{pre}" WHERE nombre="{n}"')
+        # conexion.commit()
+        # conexion.close()
+        # self.obtener_productos()
+
 
         
         
@@ -88,35 +138,97 @@ class Aplicacion(Frame):
 
         
         
-    def crear_componentes(self):
-        GLabel_654=tk.Label()
-        GLabel_654["bg"] = "#6ea8d3"
-        GLabel_654["cursor"] = "spider"
-        ft = tkFont.Font(family='Times',size=16)
-        GLabel_654["font"] = ft
-        GLabel_654["fg"] = "#000000"
-        GLabel_654["justify"] = "center"
-        GLabel_654["text"] = "Nombre: "
-        GLabel_654.place(x=110,y=100,width=93,height=31)
+    def crear_componentes(self):    
+        
+        GLabel_464=tk.Label()
+        GLabel_464["bg"] = "#90ee90"
+        ft = tkFont.Font(family='Times',size=17)
+        GLabel_464["font"] = ft
+        GLabel_464["fg"] = "#333333"
+        GLabel_464["justify"] = "center"
+        GLabel_464["text"] = "A D M I N I S T R A D O R"
+        GLabel_464.place(relx=0.37,rely=0.05,width=263,height=30)
 
-        GLabel_344=tk.Label()
-        GLabel_344["bg"] = "#598ebc"
-        ft = tkFont.Font(family='Times',size=16)
-        GLabel_344["font"] = ft
-        GLabel_344["fg"] = "#010101"
-        GLabel_344["justify"] = "center"
-        GLabel_344["text"] = "Precio:"
-        GLabel_344.place(x=110,y=160,width=93,height=30)
-
-        self.txtNombre=tk.Entry()
-        self.txtNombre["borderwidth"] = "1px"
+        GLabel_720=tk.Label()
+        GLabel_720["bg"] = "#75afda"
         ft = tkFont.Font(family='Times',size=10)
-        self.txtNombre["font"] = ft
-        self.txtNombre["fg"] = "#333333"
-        self.txtNombre["justify"] = "left"
-        self.txtNombre["text"] = "nombre"
-        self.txtNombre.focus()
-        self.txtNombre.place(x=230,y=100,width=203,height=30)
+        GLabel_720["font"] = ft
+        GLabel_720["fg"] = "#333333"
+        GLabel_720["justify"] = "center"
+        GLabel_720["text"] = "•marca• :"
+        GLabel_720.place(x=400,y=70,width=78,height=30)
+
+        GLabel_380=tk.Label()
+        GLabel_380["bg"] = "#75afda"
+        ft = tkFont.Font(family='Times',size=10)
+        GLabel_380["font"] = ft
+        GLabel_380["fg"] = "#333333"
+        GLabel_380["justify"] = "center"
+        GLabel_380["text"] = "•Precio• :"
+        GLabel_380.place(x=590,y=70,width=82,height=30)
+
+        GLabel_492=tk.Label()
+        GLabel_492["bg"] = "#75afda"
+        ft = tkFont.Font(family='Times',size=10)
+        GLabel_492["font"] = ft
+        GLabel_492["fg"] = "#333333"
+        GLabel_492["justify"] = "center"
+        GLabel_492["text"] = "•descripcion• :"
+        GLabel_492.place(x=400,y=110,width=82,height=30)
+
+        GLabel_994=tk.Label()
+        GLabel_994["bg"] = "#75afda"
+        ft = tkFont.Font(family='Times',size=10)
+        GLabel_994["font"] = ft
+        GLabel_994["fg"] = "#333333"
+        GLabel_994["justify"] = "center"
+        GLabel_994["text"] = "•cantidad• :"
+        GLabel_994.place(x=590,y=110,width=80,height=30)
+
+        GLabel_866=tk.Label()
+        GLabel_866["bg"] = "#75afda"
+        ft = tkFont.Font(family='Times',size=10)
+        GLabel_866["font"] = ft
+        GLabel_866["fg"] = "#333333"
+        GLabel_866["justify"] = "center"
+        GLabel_866["text"] = "•Id• :"
+        GLabel_866.place(x=210,y=70,width=77,height=30)
+
+        GLabel_841=tk.Label()
+        GLabel_841["bg"] = "#75afda"
+        ft = tkFont.Font(family='Times',size=10)
+        GLabel_841["font"] = ft
+        GLabel_841["fg"] = "#333333"
+        GLabel_841["justify"] = "center"
+        GLabel_841["text"] = "•Nombre• :"
+        GLabel_841.place(x=210,y=110,width=76,height=30)
+
+        self.id=tk.Entry()
+        self.id["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        self.id["font"] = ft
+        self.id["fg"] = "#333333"
+        self.id["justify"] = "left"
+        self.id["text"] = "id"
+        self.id.place(x=300,y=70,width=89,height=30)
+
+        self.marca=tk.Entry()
+        self.marca["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        self.marca["font"] = ft
+        self.marca["fg"] = "#333333"
+        self.marca["justify"] = "left"
+        self.marca["text"] = "marca"
+        self.marca.place(x=490,y=70,width=88,height=30)
+
+        self.descripcion=tk.Entry()
+        self.descripcion["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        self.descripcion["font"] = ft
+        self.descripcion["fg"] = "#333333"
+        self.descripcion["justify"] = "left"
+        self.descripcion["text"] = "descripcion"
+        self.descripcion.place(x=490,y=110,width=88,height=30)
 
         self.precio=tk.Entry()
         self.precio["borderwidth"] = "1px"
@@ -125,7 +237,56 @@ class Aplicacion(Frame):
         self.precio["fg"] = "#333333"
         self.precio["justify"] = "left"
         self.precio["text"] = "precio"
-        self.precio.place(x=230,y=160,width=201,height=30)
+        self.precio.place(x=690,y=70,width=89,height=30)
+
+        self.cantidad=tk.Entry()
+        self.cantidad["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        self.cantidad["font"] = ft
+        self.cantidad["fg"] = "#333333"
+        self.cantidad["justify"] = "left"
+        self.cantidad["text"] = "cantidad"
+        self.cantidad.place(x=690,y=110,width=91,height=30)
+
+        self.nombre=tk.Entry()
+        self.nombre["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        self.nombre["font"] = ft
+        self.nombre["fg"] = "#333333"
+        self.nombre["justify"] = "left"
+        self.nombre["text"] = "nombre"
+        self.nombre.place(x=300,y=110,width=89,height=30)
+
+
+
+        # boton=tk.Button(text="hola")
+        # boton.place(relx=0.12,rely=0.48)
+        self.tabla=ttk.Treeview(height=12,columns=("nombre","marca","descripcion","precio","cantidad"))
+
+        self.tabla.column("#0",width=60)
+        self.tabla.column("nombre",width=150)
+        self.tabla.column("marca",width=120)
+        self.tabla.column("descripcion",width=180)
+        self.tabla.column("precio",width=60)
+        self.tabla.column("cantidad",width=70)
+
+
+
+        self.tabla.heading("#0",text="id",anchor=CENTER)
+        self.tabla.heading("nombre",text="nombre",anchor=CENTER)
+        self.tabla.heading("marca",text="marca",anchor=CENTER)
+        self.tabla.heading("descripcion",text="descripcion",anchor=CENTER)
+        self.tabla.heading("precio",text="precio",anchor=CENTER)
+        self.tabla.heading("cantidad",text="cantidad",anchor=CENTER)
+
+        self.tabla.insert("","end",text=1,values=("chocolate","cofler block","chocolate de 90 gr",120,333))
+        self.tabla.place(relx=0.06, rely=0.50,relwidth=0.90)
+
+        # self.mensaje=tk.Label(text="", fg="red")
+        # self.mensaje.place(x=230 ,y=130)
+
+
+        
 
         agregar_producto=tk.Button()
         agregar_producto["bg"] = "#90ee90"
@@ -134,27 +295,9 @@ class Aplicacion(Frame):
         agregar_producto["fg"] = "#000000"
         agregar_producto["justify"] = "center"
         agregar_producto["text"] = "Agregar Producto"
-        agregar_producto.place(x=210,y=220,width=191,height=30)
+        agregar_producto.place(x=210,rely=0.30,width=191,height=30)
         agregar_producto["command"] = self.agregar_productos
 
-        GLabel_464=tk.Label()
-        GLabel_464["bg"] = "#90ee90"
-        ft = tkFont.Font(family='Times',size=17)
-        GLabel_464["font"] = ft
-        GLabel_464["fg"] = "#333333"
-        GLabel_464["justify"] = "center"
-        GLabel_464["text"] = "Añadir Producto"
-        GLabel_464.place(x=180,y=40,width=263,height=30)
-
-        # boton=tk.Button(text="hola")
-        # boton.place(relx=0.12,rely=0.48)
-        self.tabla=ttk.Treeview(height=12,columns=2)
-        self.tabla.heading("#0",text="Nombre",anchor=CENTER)
-        self.tabla.heading("#1",text="precio",anchor=CENTER )
-        self.tabla.place(relx=0.20, rely=0.60)
-
-        self.mensaje=tk.Label(text="", fg="red")
-        self.mensaje.place(x=230 ,y=130)
 
         GButton_483=tk.Button()
         GButton_483["activebackground"] = "#d22b2b"
@@ -165,7 +308,7 @@ class Aplicacion(Frame):
         GButton_483["fg"] = "#fcfcfc"
         GButton_483["justify"] = "center"
         GButton_483["text"] = "Eliminar"
-        GButton_483.place(x=80,y=270,width=208,height=30)
+        GButton_483.place(relx=.40,rely=0.30,width=208,height=30)
         GButton_483["command"] = self.eliminar_productos
 
         GButton_55=tk.Button()
@@ -177,22 +320,7 @@ class Aplicacion(Frame):
         GButton_55["text"] = "Actulizar"
         GButton_55.place(relx=0.65,rely=0.30,width=220,height=30)
         GButton_55["command"] = self.editar_productos
-
-        t_nombre=tk.Label()
-        t_nombre['text']="ingrese un nuevo nombre"
-        t_nombre.place(relx=0.67,rely=0.12)
-
-        self.n_nombre=tk.Entry()
-        self.n_nombre.place(relx=0.67,rely=0.15, width=180,height=20)
-
-        t_precio=tk.Label()
-        t_precio["text"]="Ingrese un nuevo precio"
-        t_precio.place(relx=0.67,rely=0.20)
-
-        self.n_precio=tk.Entry()
-        self.n_precio.place(relx=0.67, rely=0.23,width=180,height=20)
-
-    
+       
 
 
     # def agregar_producto_command(self):
@@ -205,6 +333,7 @@ class Aplicacion(Frame):
 
 principal=Tk()
 principal.wm_title("Producto")
-principal.geometry("720x640")
+principal.geometry("1080x800")
+principal.config(bg="black")
 marco=Aplicacion(principal)
 principal.mainloop()
--- prueba.py
+++ prueba.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conexion=sqlite3.connect("12.db ")
+conexion=sqlite3.connect("12.db")
 cursor=conexion.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS producto (id INTEGER NOT NULL,nombre TEXT,precio REAL,PRIMARY KEY (id AUTOINCREMENT)) ") 
 conexion.commit()
